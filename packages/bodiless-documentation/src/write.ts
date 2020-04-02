99e6bdc93d436feba54136b758609408f6039884
Konstantin Mishur <kmishur@its.jnj.com>
2020-04-02 14:47:32 +0300

    temp: add some logs again

--- packages/bodiless-documentation/src/write.ts
+++ packages/bodiless-documentation/src/write.ts
@@ -32,6 +32,10 @@ type Copier = (docPath: string, filePath: string) => Promise<void>;
  * @param filePath The path of the target document relative to `process.cwd`
  */
 const copyFile: Copier = (docPath: string, filePath: string) => {
+  if (filePath !== 'doc/README.md') {
+    return Promise.resolve();
+  }
+  console.log('copy file', docPath, filePath);
   const relPath = path.relative(process.cwd(), docPath);
   try {
     return fs.copy(relPath, filePath, { overwrite: false });
