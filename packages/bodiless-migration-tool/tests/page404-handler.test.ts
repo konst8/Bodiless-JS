f95d9e99f9199dea46f05db6a5e7ec28f24b0710
Konstantin Mishur <kmishur@its.jnj.com>
2020-04-21 14:48:31 +0300

    Complete unit tests

--- packages/bodiless-migration-tool/tests/page404-handler.test.ts
+++ packages/bodiless-migration-tool/tests/page404-handler.test.ts
@@ -42,8 +42,45 @@ describe('page404Handler.getParams', () => {
   });
 });
 
-// @ TBD
-// describe('page404Handler.processScrapedPage', () => {
-//   it('should ...', () => {
-//   });
-// });
+describe('page404Handler.processScrapedPage', () => {
+  const input = {
+    pageUrl: 'https://example.com/custom-404',
+    rawHtml: '',
+    status: 404,
+    processedHtml: '',
+    metatags: [],
+    scripts: [],
+    inlineScripts: [],
+    styles: [],
+    links: [],
+    inlineStyles: [],
+    images: [],
+  };
+  it('should do nothing if page 404 functionality is disabled', () => {
+    const page404params = {
+      isPage404Disabled: true,
+      page404Url: 'https://example.com/404',
+    };
+    const expected = input;
+    expect(page404Handler.processScrapedPage(input, page404params)).toEqual(expected);
+  });
+  it(`should change page url to /404 so that page creator always uses /404 path even for custom
+"page not found" pages`, () => {
+    const page404params = {
+      isPage404Disabled: false,
+      page404Url: 'https://example.com/custom-404',
+    };
+    const expected = 'https://example.com/404';
+    expect(page404Handler.processScrapedPage(input, page404params).pageUrl).toBe(expected);
+  });
+  it(`should throw an error if current page responses with 404 status,
+but it is not a default 404 page`, () => {
+    const page404params = {
+      isPage404Disabled: false,
+      page404Url: 'https://example.com/another-default-404',
+    };
+    expect(() => page404Handler.processScrapedPage(input, page404params)).toThrow(
+      `Page ${input.pageUrl} does not exist and will be redirected to default 404 page.`,
+    );
+  });
+});
